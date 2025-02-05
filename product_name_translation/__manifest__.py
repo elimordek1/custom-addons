469de150de304417177befea5530ac9436ef120e
elimordek1 <86974083+elimordek1@users.noreply.github.com>
2025-02-05 13:33:22 +0400

    Add files via upload

---
+++ product_name_translation/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Product Name Translation',
+    'version': '1.0',
+    'category': 'Sales',
+    'author': 'Your Company',
+    'depends': ['product'],
+    'data': [
+        'views/product_template_view.xml',
+    ],
+    'installable': True,
+}
